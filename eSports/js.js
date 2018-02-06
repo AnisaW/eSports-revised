768442175b6ab80e5c6bd2e464c4c27fc7a52b85
Keith Tang <keithsktang@gmail.com>
2018-02-06 00:14:11 -0500

    update hovering behavior and fixed file structure

--- eSports/js.js
+++ eSports/js.js
@@ -1,16 +1,16 @@
 
-$(document).read(setTimeout(function(){
+$(document).ready(setTimeout(function(){
   $(".explained").removeClass("hide");
   $(".what-is-it").addClass("fadeOut");
   $(".who-plays-it").addClass("fadeOut");
-  $(".explained").addClass("fadeInUpBig");
+  $(".explained").addClass("fadeIn");
   setInterval(explained, 20000);
 }, 5000))
 
 $("#esports-logo").click(explained);
 
 function explained() {
-  $(".explained").toggleClass("fadeInDownBig fadeOut");
-  $(".what-is-it").toggleClass("fadeOut fadeInDownBig");
-  $(".who-plays-it").toggleClass("fadeOut fadeInUpBig");
+  $(".explained").toggleClass("fadeIn fadeOut");
+  $(".what-is-it").toggleClass("fadeOut fadeIn");
+  $(".who-plays-it").toggleClass("fadeOut fadeIn");
 }
